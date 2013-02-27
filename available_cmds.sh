1e8fcdf67906fec8dcd8efe21a45376d382d09dc
Eric Gallager <egall@gwmail.gwu.edu>
2013-02-26 22:09:38 -0500

    add some flags to available_cmds.sh

--- available_cmds.sh
+++ available_cmds.sh
@@ -6,16 +6,32 @@ export NUMBER_OF_PATH_DIRS=$(echo $PATH | tr : \\n | wc -l)
 export TOTAL_LINES=$(echo $PATH | tr : \\n | xargs ls -1p | wc -l)
 
 printf "\n"
-echo "Available commands:"
-printf "\n"
+if [ "$1" == "--help" ]; then
+	echo "Usage:"
+	echo "available_cmds.sh [-v] [-q]"
+	echo "-v: This flag prints the full path of all found commands."
+	echo "-q: This flag keeps the output machine-readable."
+	printf "\n"
+	exit
+fi
+if [ "$1" != "-q" ]; then
+	echo "Available commands:"
+	printf "\n"
+fi
 
 export AVAILABLE_COMMANDS=$(echo $PATH | tr : \\n | xargs ls -1p | sort | tail -n $(($TOTAL_LINES - $((2 * $NUMBER_OF_PATH_DIRS)))) | uniq | tr \\n \ )
 
 for command in $AVAILABLE_COMMANDS ; do
 	if [ -x `which ${command}` ]; then
-		printf "${command} "
+		if [ "$1" == "-v" ]; then
+			which ${command}
+		else
+			printf "${command} "
+		fi
 	fi
 done
 
-printf "\n"
+if [ "$1" != -v ]; then
+	printf "\n"
+fi
 printf "\n"
