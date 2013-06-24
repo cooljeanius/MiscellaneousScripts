b828fe9f2c681cbb4002059f9aa519fe67f237a9
Eric Gallager <egallager@comcast.net>
2013-06-24 16:56:22 -0300

    Update useless.sh

--- useless.sh
+++ useless.sh
@@ -10,25 +10,25 @@ fi
 pwd
 whoami
 cat $0
-if [ -z "`ps -c | grep "bash"`" ]; then
+if [ -z "$(ps -c | grep "bash")" ]; then
 	ps
 	ps | wc
 	time bash $0
 	ps
 	ps | wc
-elif [ -z "`ps -c | grep "zsh"`" ]; then
+elif [ -z "$(ps -c | grep "zsh")" ]; then
 	ps
 	ps | wc
 	time zsh $0
 	ps
 	ps | wc
-elif [ -z "`ps -c | grep "ksh"`" ]; then
+elif [ -z "$(ps -c | grep "ksh")" ]; then
 	ps
 	ps | wc
 	time ksh $0
 	ps
 	ps | wc
-elif [ -z "`ps -c | grep "sh"`" ]; then
+elif [ -z "$(ps -c | grep "sh")" ]; then
 	ps
 	ps | wc
 	time sh $0
