16de07eab6a5275e77a277882671861ef2dfa5dd
Eric Gallager <egallager@comcast.net>
2013-06-24 16:52:45 -0300

    Update hideCVSfolders.sh

--- hideCVSfolders.sh
+++ hideCVSfolders.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-#TODO: escape spaces in paths properly
 
-for i in `find * | grep CVS`; do 
-	if [ -d ${i} ]; then 
+if [ ! -x `which chflags` ]; then
+	echo "This script only works on platforms with a working chflags."
+	exit 1
+fi
+
+echo "Running $0 in `pwd`..."
+echo "(taking a directory as an argument has yet to be implemented)"
+#TODO: get spaces in paths to escape properly (the current method doesn't work...)
+for i in $(echo "$(find * | grep CVS | sed 's/ /\\ /g')"); do 
+	# This echo statement can be removed once spaces are escaped properly
+	echo "${i}"
+	if [ -d ${i} ]; then
+		echo "Hiding ${i}..."
 		chflags hidden ${i};
 	fi;
 done
