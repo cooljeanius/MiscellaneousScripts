8c0247046ed53e2d908454eaccfc943f736b80b4
Eric Gallager <egall@gwmail.gwu.edu>
2025-09-17 11:09:27 -0400

    Create cheat_at_kusari.sh
    New script
    intended for use with:
    https://github.com/jurienhamaker/Yugen

---
+++ cheat_at_kusari.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# cheat_at_kusari.sh: get distribution of words starting with a certain letter
+# by the letter they end with
+
+printf "\n"
+if [ "$1" == "--help" ] || [ -z "$1" ]; then
+	echo "Usage:"
+	echo "cheat_at_kusari.sh [letter]"
+	printf "\n"
+	exit
+fi
+
+for letter in $(perl -E 'say for a..z'); do
+    echo "$1...${letter}: $(grep -e "^$1.*${letter}$" /usr/share/dict/words | wc -l)";
+done
+
+# TODO: calculate which ending letter has the lowest nonzero total
+# then do `grep -e "^$1.*${that_letter}$" /usr/share/dict/words` for it
